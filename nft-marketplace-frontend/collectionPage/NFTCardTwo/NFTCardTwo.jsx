8bd9c6d2c7b1f94a7fac3c998f05bc3df5569e5c
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-17 23:31:48 +0700

    add some module in NFTCardTwo (box, stock)

--- nft-marketplace-frontend/collectionPage/NFTCardTwo/NFTCardTwo.jsx
+++ nft-marketplace-frontend/collectionPage/NFTCardTwo/NFTCardTwo.jsx
@@ -45,6 +45,7 @@ const NFTCardTwo = ({ NFTData }) => {
                         width={500}
                         height={500}
                         objectFit="cover"
+                        className={Style.NFTCardTwo_box_img_img}
                     />
                 </div>
 
@@ -59,7 +60,7 @@ const NFTCardTwo = ({ NFTData }) => {
                 <div className={Style.NFTCardTwo_box_price}>
                     <div className={Style.NFTCardTwo_box_price_box}>
                         <small>Current Bid</small>
-                        <p>1{i + 5}</p>
+                        <p>1{i + 5}.000 ETH</p>
                     </div>
                     <p className={Style.NFTCardTwo_box_price_stock}>
                         <MdTimer />
--- nft-marketplace-frontend/pages/collection.js
+++ nft-marketplace-frontend/pages/collection.js
@@ -24,7 +24,7 @@ const collection = () => {
         <Banner bannerImage={images.creatorbackground1} />
         <collectionProfile />
         <NFTCardTwo NFTData={collectionArray} />
-        <Filter />
+       
         <Slider />
         <Brand />
     </div>
