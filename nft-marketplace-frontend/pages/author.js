214fcfff6ce7a3645c6df58ddbdedf6472bce307
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-18 12:24:40 +0700

    add author profile card, author taps in AuthorPage

--- nft-marketplace-frontend/pages/author.js
+++ nft-marketplace-frontend/pages/author.js
@@ -3,12 +3,31 @@ import React, {useState, useEffect } from 'react'
 import Style from "../styles/author.module.css";
 import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
 import { Brand, Title } from "../components/componentsindex";
-
+import images from '../img';
+import { AuthorProfileCard, AuthorTaps, TabCard } from '../authorPage/componentsIndex';
 
 const author = () => {
-  return (
-    <div>author</div>
-  )
+    const popularArray = [
+        images.user1,
+        images.user2,
+        images.user3,
+        images.user4,
+        images.user5,
+        images.user6,
+        images.user7,
+        images.user8,
+    ]
+    const [collectiables, setCollectiables] = useState(true);
+    const [created, setCreated] = useState(false);
+    const [like, setLike] = useState(false);
+    const [follower, setFollower] = useState(false);
+    const [following, setFollowing] = useEffect(false);
+    return (
+        <div className={Style.banner}>
+            <Banner bannerImage={images.creatorbackground2} />
+            <AuthorProfileCard />
+        </div>
+    )
 }
 
 export default author
