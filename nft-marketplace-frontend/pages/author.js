d261513defac1101ef2fda208649dc610f4cd123
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-18 12:11:04 +0700

    add authorPage (file .js and folder)

---
+++ nft-marketplace-frontend/pages/author.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect } from 'react'
+
+import Style from "../styles/author.module.css";
+import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
+import { Brand, Title } from "../components/componentsindex";
+
+
+const author = () => {
+  return (
+    <div>author</div>
+  )
+}
+
+export default author
