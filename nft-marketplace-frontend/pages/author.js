bd391165de820f9dfcaa89ed30eaa8ed73d7b585
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-18 14:25:02 +0700

    modify author profile card. module.css

--- nft-marketplace-frontend/pages/author.js
+++ nft-marketplace-frontend/pages/author.js
@@ -1,33 +1,50 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 import Style from "../styles/author.module.css";
 import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
 import { Brand, Title } from "../components/componentsindex";
-import images from '../img';
-import { AuthorProfileCard, AuthorTaps, TabCard } from '../authorPage/componentsIndex';
+import images from "../img";
+import FollowerTab from "../components/componentsindex";
+import {
+  AuthorProfileCard,
+  AuthorTaps,
+  TabCard,
+} from "../authorPage/componentsIndex";
 
 const author = () => {
-    const popularArray = [
-        images.user1,
-        images.user2,
-        images.user3,
-        images.user4,
-        images.user5,
-        images.user6,
-        images.user7,
-        images.user8,
-    ]
-    const [collectiables, setCollectiables] = useState(true);
-    const [created, setCreated] = useState(false);
-    const [like, setLike] = useState(false);
-    const [follower, setFollower] = useState(false);
-    const [following, setFollowing] = useState(false);
-    return (
-        <div className={Style.banner}>
-            <Banner bannerImage={images.creatorbackground2} />
-            <AuthorProfileCard />
-        </div>
-    )
-}
+  const popularArray = [
+    images.user1,
+    images.user2,
+    images.user3,
+    images.user4,
+    images.user5,
+    images.user6,
+    images.user7,
+    images.user8,
+  ];
+  const [collectiables, setCollectiables] = useState(true);
+  const [created, setCreated] = useState(false);
+  const [like, setLike] = useState(false);
+  const [follower, setFollower] = useState(false);
+  const [following, setFollowing] = useState(false);
+  return (
+    <div className={Style.banner}>
+      <Banner bannerImage={images.creatorbackground2} />
+      <AuthorProfileCard />
+      <AuthorTaps
+        collectiables={collectiables}
+        created={created}
+        like={like}
+        follower={follower}
+      />
+      <Title
+        heading="Popular Creator"
+        paragraph="Click on music icon and enjoy NFT music or audio"
+      />
 
-export default author
+      <Brand />
+    </div>
+  );
+};
+
+export default author;
