43f9524e0973c57d51b7d43baa5d25ddaa27b0a3
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-21 01:30:26 +0700

    remove .footer_box_discover&reload click Discover

--- nft-marketplace-frontend/components/Navbar/Discover/Discover.jsx
+++ nft-marketplace-frontend/components/Navbar/Discover/Discover.jsx
@@ -42,13 +42,20 @@ const Discover = () => {
   ];
   return (
     <div>
-      {discover.map((el, i) => (
+      {/* {discover.map((el, i) => (
         <div key={i + 1} className={Style.discover}>
           <Link href={{ pathname: `${el.link}` }} >
             {el.name}
           </Link>
         </div>
-      ))}
+      ))} */}
+      <div>
+        {discover.map((el, i) => (
+          <div key={i + 1} className={Style.discover}>
+            <a href={`/${el.link}`}>{el.name}</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
