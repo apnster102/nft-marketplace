d3e6921206e8a4a0d61b318d916a81532cb2ecb9
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-21 03:05:38 +0700

    contactus chưa modify css

--- nft-marketplace-frontend/pages/contactUS.js
+++ nft-marketplace-frontend/pages/contactUS.js
@@ -2,84 +2,87 @@ import React from "react";
 import {
   TiSocialFacebook,
   TiSocialLinkedin,
+  TiSocialTwitter,
   TiSocialYoutube,
   TiSocialInstagram,
-  TiArrowSortedDown,
-  TiArrowSortedUp,
 } from "react-icons/ti";
-import { HiOutLineMail } from "react-icons/hi";
-import Style from "../styles/contactUs.module.css";
+import { HiOutlineMail } from "react-icons/hi";
+import Style from "../styles/contactus.module.css";
 import formStyle from "../components/AccountPage/Form/Form.module.css";
 import { Button } from "../components/componentsindex";
 
-const contactUS = () => {
+const contactus = () => {
   return (
-    <div className={formStyle.contactus}>
-      <div className={formStyle.contactus_box}>
+    <div className={Style.contactus}>
+      <div className={Style.contactus_box}>
         <h1>Contact</h1>
-        <div className={formStyle.contactus_box_box}>
-          <div className={formStyle.contactus_box_box_left}>
-            <div className={formStyle.contactus_box_box_left_item}>
-              <h3>Address</h3>
-              <p>G2, DH Cong Nghe, 144 Xuan Thuy, Cau Giay, HN</p>
+        <div className={Style.contactus_box_box}>
+          <div className={Style.contactus_box_box_left}>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>ADDRESS</h3>
+              <p>
+                G2, University of Engineering and Technology, 144 Xuan Thuy, Cau
+                Giay, HN
+              </p>
             </div>
-            <div className={formStyle.contactus_box_box_left_item}>
-              <h3> Email</h3>
-              <p> MSSV@vnu.edu.vn</p>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>EMAIL</h3>
+              <p>studentid@vnu.edu.vn</p>
             </div>
-            <div className={formStyle.contactus_box_box_left_item}>
-              <h3> Phone</h3>
-              <p> 085-123-1234</p>
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>PHONE</h3>
+              <p>085-123-9999</p>
             </div>
-            <div className={formStyle.contactus_box_box_left_item}>
-              <h3> Social</h3>
-              <a href="https://www.facebook.com/" target="_blank">
+            <div className={Style.contactus_box_box_left_item}>
+              <h3>SOCIALS</h3>
+              <a href="#">
                 <TiSocialFacebook />
               </a>
-              <a href="https://www.instagram.com/?hl=en" target="_blank">
-                <TiSocialInstagram />
-              </a>
-              <a href="https://www.linkedin.com/" target="_blank">
+              <a href="#">
                 <TiSocialLinkedin />
               </a>
-              <a href="https://www.youtube.com/" target="_blank">
+              <a href="#">
+                <TiSocialInstagram />
+              </a>
+              <a href="#">
                 <TiSocialYoutube />
+              </a>
+              <a href="#">
+                <TiSocialTwitter />
               </a>
             </div>
           </div>
-          <div className={formStyle.contactus_box_box_right}>
+          <div className={Style.contactus_box_box_right}>
             <form>
-              {/* user Name */}
-              <div className={formStyle.contactus_box_input}>
-                <label htmlFor="name"> Username</label>
+              <div className={formStyle.Form_box_input}>
+                <label htmlFor="name">Full Name</label>
                 <input
                   type="text"
-                  placeholder="Please enter your name"
+                  placeholder="dnaxxdy"
                   className={formStyle.Form_box_input_userName}
                 />
               </div>
-              {/* Email */}
-              <div className={formStyle.contactus_box_input}>
-                <label htmlFor="email"> Email</label>
+              <div className={formStyle.Form_box_input}>
+                <label htmlFor="email">Email</label>
                 <div className={formStyle.Form_box_input_box}>
-                  <div className={Form_box_input_box_icon}>
-                    <HiOutLineMail />
+                  <div className={formStyle.Form_box_input_box_icon}>
+                    <HiOutlineMail />
                   </div>
+                  <input type="text" placeholder="Email*" />
                 </div>
               </div>
-              {/* Description */}
               <div className={formStyle.Form_box_input}>
-                <label htmlFor="description">Description</label>
+                <label htmlFor="description">Message</label>
                 <textarea
                   name=""
                   id=""
-                  cols={30}
-                  rows={6}
-                  placeholder="Something about your self in few words"
+                  cols="30"
+                  rows="6"
+                  placeholder="something about yourself in few words"
                 ></textarea>
               </div>
               <Button
-                btnName="Send message"
+                btnName="Send Message"
                 handleClick={() => {}}
                 classStyle={Style.button}
               />
@@ -91,4 +94,4 @@ const contactUS = () => {
   );
 };
 
-export default contactUS;
+export default contactus;
