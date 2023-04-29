42c0157c8a08829ba2169e5b8bceef0a27fc2786
nguyetanhdam <20020129@vnu.edu.vn>
2023-04-29 21:01:46 +0700

    commit

--- nft-marketplace-frontend/components/Navbar/Profile/Profile.jsx
+++ nft-marketplace-frontend/components/Navbar/Profile/Profile.jsx
@@ -23,7 +23,7 @@ const Profile = ({ currentAccount }) => {
 
         <div className={Style.profile_account_info}>
           <p>Team 22</p>
-          <small>{currentAccount.slice(0, 18)}..</small>
+          <small>{currentAccount.slice(0, 18)}INT3121 20</small>
         </div>
       </div>
 
